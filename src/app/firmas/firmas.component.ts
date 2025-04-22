abfb79147a1555efbf58dff7e09c04f380e0a2a9
chava28pvp <129213697+chava28pvp@users.noreply.github.com>
2025-04-22 16:43:26 -0600

    my first commit

---
+++ src/app/firmas/firmas.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-firmas',
+  standalone: true,
+  imports: [],
+  templateUrl: './firmas.component.html',
+  styleUrl: './firmas.component.css'
+})
+export class FirmasComponent {
+
+}
